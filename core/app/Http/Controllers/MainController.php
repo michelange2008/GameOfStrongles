8f114f95a09542ce309b7b3b9b511b57884e1396
michelange2008 <michelange@wanadoo.fr>
2018-11-27 13:40:39 +0100

    intermédiaire

--- core/app/Http/Controllers/MainController.php
+++ core/app/Http/Controllers/MainController.php
@@ -3,43 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Carbon\Carbon;
-
 use App\Models\Troupeau;
 use App\Models\Parcelle;
-use App\Models\Dessinparcelle;
-use App\Models\Dessinparcellaire;
-use App\Models\StrongleIn;
-use App\Models\StrongleOut;
 use App\Traits\NbLignes;
-use App\Traits\ListeMois;
 
 class MainController extends Controller
 {
   use NbLignes;
-  use ListeMois;
 
     public function index()
     {
-      //################### Données temporairesp pour créer les objets #########
-      $troupeau_exemple = ['caprins', 50];
-      $listes_parcelles = collect([
-        ['nom' => 'petit champ', 'superficie' => 4],
-        ['nom' => 'grand pré', 'superficie' => 8],
-        ['nom' => 'chez Marcel', 'superficie' => 0.5],
-        ['nom' => 'en-bas', 'superficie' => 3],
-        ['nom' => 'en-haut', 'superficie' => 10],
-      ]);
-      $nb_strongles_initial = 1;
-      $mois = 3;
-      $jour = 18;
-      $duree_paturage = 200; // nombre de jours
-      $mise_a_l_herbe = Carbon::createFromDate(Carbon::now()->year, $mois, $jour);
-      $liste_mois = $this->listeMois($mise_a_l_herbe, $duree_paturage);
-      // dd($liste_mois);
-      //########################################################################
+      $troupeau = new Troupeau('caprins', 50);
 
-      //################## Création des lots de strongles ######################
       $nb_lots = 100;
       $nb_parasite = 5;
       for($i = 0; $i < $nb_lots ; $i++)
@@ -51,26 +26,26 @@ class MainController extends Controller
         }
         $total_parasite[$i] = $lot;
       }
-      //########################################################################
 
-      //#################### CREATION D'UN NOUVEAU TROUPEAU ####################
-      $troupeau = new Troupeau($troupeau_exemple[0], $troupeau_exemple[1]);
-      $troupeau->setInfestation($nb_strongles_initial);
-
-      //################## CREATION DE LA LISTE DES PARCELLES À DESSINER ######################
-      $dessinparcellaire = new Dessinparcellaire(); // Nouvel objet dessinParcellaire
-      for($i = 0; $i < $listes_parcelles->count(); $i++) { // On boucle sur la liste des parcelles
-        $parcelle = new Parcelle($listes_parcelles[$i]['nom'], $listes_parcelles[$i]['superficie']);
-        $parcelle->setInfestation($nb_strongles_initial);
-        $dessinparcelle = new Dessinparcelle($i, $parcelle); // On crée un objet dessinparcelle avec chaque parcelle
-        $dessinparcellaire->addDessinparcelle($dessinparcelle); // On ajoute cet objet à la liste de l'objet dessinParcellaire
+      $listes_parcelles = collect([
+        ['nom' => 'petit champ', 'superficie' => 4],
+        ['nom' => 'grand pré', 'superficie' => 8],
+        ['nom' => 'chez Marcel', 'superficie' => 0.5],
+        ['nom' => 'en-bas', 'superficie' => 3],
+        ['nom' => 'en-haut', 'superficie' => 10],
+      ]);
+      $longueurs_cotes = $listes_parcelles->sum('longueur_cote');
+      $parcelles = collect();
+      $i = 0;
+      foreach ($listes_parcelles as $parcelle) {
+        $parcelles->push(new Parcelle($i, $parcelle['nom'], $parcelle['superficie'] ));
+        $i++;
       }
-      $dessinparcellaire->SetLongueur_et_X_DessinParcelle(); // On fixe les valeurs de X (position dans la page) et de longueur des objets dessinparcelle
-// dd($dessinparcellaire);
+      $nb_lignes = $this->nblignes($parcelles->count());
+      dd($nb_lignes);
       return view('gos_main', [
-        'liste_mois' => $liste_mois,
-        'troupeau' => $troupeau,
-        'liste_parcelles' => $dessinparcellaire,
+        'parcelles' => $parcelles,
+        'total_parasite' => $total_parasite,
       ]);
     }
 
